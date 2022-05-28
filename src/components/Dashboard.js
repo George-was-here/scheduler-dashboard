bb2ac76950dce5841cb6b96a0bb5d0f4a85315e6
George Burt <georgeburt@Georges-MacBook-Air.local>
2022-05-28 00:32:12 -0400

    Passing props to Panels

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,10 +2,41 @@ import React, { Component } from "react";
 
 import Loading from "./Loading";
 
+import Panel from "./Panel";
+
 import classnames from "classnames";
 
+const data = [
+  {
+    id: 1,
+    label: "Total Interviews",
+    value: 6
+  },
+  {
+    id: 2,
+    label: "Least Popular Time Slot",
+    value: "1pm"
+  },
+  {
+    id: 3,
+    label: "Most Popular Day",
+    value: "Wednesday"
+  },
+  {
+    id: 4,
+    label: "Interviews Per Day",
+    value: "2.3"
+  }
+];
+
 class Dashboard extends Component {
-  state = { loading: false }
+  constructor(props) {
+    super(props);
+    console.log(props)
+    this.state = { loading: false }
+  }
+  
+
 
   render() {
     const dashboardClasses = classnames("dashboard");
@@ -14,7 +45,12 @@ class Dashboard extends Component {
       return <Loading />;
     }
 
-    return <main className={dashboardClasses} />;
+    return (
+    <main className={dashboardClasses}>
+        {data.map(panel => <Panel key={panel.id} id={panel.id} label={panel.label} value={panel.value} />)}
+
+     </main>
+    )
   }
 }
 
